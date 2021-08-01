d2ae9101f88997956aae7f29b6603c1e23b112f2
aryalp2 <aryalp2@gmail.com>
2021-08-01 17:15:45 +0545

    Create Sample products in mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
-import data from "./data.js";
 import cors from "cors";
 import userRouter from "./routers/userRouter.js";
+import productRouter from "./routers/productRouter.js";
 const app = express();
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
 	useNewUrlParser: true,
@@ -13,23 +13,12 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000;
-app.get("/", (req, res) => {
-	res.send("Server is ready");
-});
-
-app.get("/api/products", (req, res) => {
-	res.status(200).send(data.products);
-});
 
 app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
 
-app.get("/api/products/:id", (req, res) => {
-	const product = data.products.find((x) => x._id === req.params.id);
-	if (product) {
-		res.status(200).send(product);
-	} else {
-		res.status(404).send({ message: "Product not found" });
-	}
+app.get("/", (req, res) => {
+	res.send("Server is ready");
 });
 
 app.use((err, req, res, next) => {
