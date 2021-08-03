0d168c9818b2f59128a72bbcaad1fb2240f32b2a
aryalp2 <aryalp2@gmail.com>
2021-08-03 15:02:19 +0545

    Create Signin backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import cors from "cors";
 import userRouter from "./routers/userRouter.js";
 import productRouter from "./routers/productRouter.js";
+
+dotenv.config();
+
 const app = express();
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
 	useNewUrlParser: true,
@@ -12,6 +16,7 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const port = process.env.PORT || 5000;
 
 app.use("/api/users", userRouter);
